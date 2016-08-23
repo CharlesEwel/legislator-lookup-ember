80becb3563a67737e2f82b1a54278943470e1a22
Alvin Ng and Charles Ewel <charles@>
2016-08-23 15:48:53 -0700

    add display for committee on bill page

--- app/helpers/subcommittee-icon.js
+++ app/helpers/subcommittee-icon.js
@@ -2,10 +2,8 @@ import Ember from 'ember';
 
 export function subcommitteeIcon(params) {
   var committee = params[0];
-  console.log(committee);
 
   if(committee.subcommittees.length > 0) {
-  console.log('in if');
     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>')
   }
 }
--- app/routes/bill.js
+++ app/routes/bill.js
@@ -5,6 +5,7 @@ export default Ember.Route.extend({
     var url = 'http://congress.api.sunlightfoundation.com/bills?apikey=b6c5123cfa5d496e97b9f3bc2c9c3608&bill_id=' + params.bill_id;
     return Ember.$.getJSON(url).then(function(responseJSON){
       return responseJSON.results[0];
+      console.log(responseJSON.results[0])
     });
   }
 });
